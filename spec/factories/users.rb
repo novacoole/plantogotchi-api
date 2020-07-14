806cf61bbe4b44289e8541163d5de7ec940661b3
nova <alexander.coole@protonmail.com>
2020-07-14 15:24:00 +1000

    added password to migration

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :user do
     username { "TestUsername" }
     email { "email@email.com" }
+    password { "validpassword" }
     bio { "This is an example bio statement." }
     location { "Location" }
     admin { false }
