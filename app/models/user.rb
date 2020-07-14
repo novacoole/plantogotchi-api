37b95a7df9e46da2120d90d96232e1bbde7570be
nova <alexander.coole@protonmail.com>
2020-07-14 15:37:57 +1000

    removed bio validation from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    validates :username, :password, :email, :bio, presence: true  
+    validates :username, :password, :email, presence: true  
     validates :username, length: { minimum: 3 }
     validates :password, length: { minimum: 8 }
     validates :email, email: true
