81ed44f71b24234647f313b1ec42382a80a65e80
nova <alexander.coole@protonmail.com>
2020-07-14 15:57:43 +1000

    added defaults to plant migration

--- db/migrate/20200714055016_create_plants.rb
+++ db/migrate/20200714055016_create_plants.rb
@@ -2,11 +2,11 @@ class CreatePlants < ActiveRecord::Migration[6.0]
   def change
     create_table :plants do |t|
       t.string :name
-      t.integer :water_level
-      t.integer :food_level
+      t.integer :water_level, :default => 100
+      t.integer :food_level, :default => 100
       t.references :breeds, null: false, foreign_key: true
-      t.boolean :alive
-      t.integer :growth_stage
+      t.boolean :alive, :default => true
+      t.integer :growth_stage, :default => 1
 
       t.timestamps
     end
