1f43ac90d54656c9035f045b683606a8c739bc67
nova <alexander.coole@protonmail.com>
2020-07-15 14:35:00 +1000

    enfore unique email in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     validates :username, :email, presence: true  
     validates :username, uniqueness: true, length: { minimum: 3 }
     has_secure_password
-    validates :email, email: true
+    validates :email, email: true, uniqueness: true
 
     has_many :plants
 end
