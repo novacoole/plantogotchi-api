5935e4b7e05f3d8209b54fd6eafdc980ebb62e4a
nova <alexander.coole@protonmail.com>
2020-07-15 11:11:08 +1000

    generated plants controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'plants/index'
+  get 'plants/show'
+  get 'plants/create'
+  get 'plants/update'
+  get 'plants/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ spec/requests/plants_request_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe "Plants", type: :request do
+
+  describe "GET /index" do
+    it "returns http success" do
+      get "/plants/index"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /show" do
+    it "returns http success" do
+      get "/plants/show"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /create" do
+    it "returns http success" do
+      get "/plants/create"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /update" do
+    it "returns http success" do
+      get "/plants/update"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /destroy" do
+    it "returns http success" do
+      get "/plants/destroy"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
