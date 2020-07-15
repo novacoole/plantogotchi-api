e877dcb5e5173dd3e1744984760f0f5d712c10bc
nova <alexander.coole@protonmail.com>
2020-07-15 13:47:19 +1000

    generated user controller, cloned plant controller logic

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,48 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :destroy]
+    
+  def index
+    users = User.all
+    render json: {users: users}, status: 200
+  end
+
+  def show
+    render json: @user, status: 200
+  end
+  
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: "plant created" , status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+  
+  def update 
+    if @user.update(plant_params)
+      render json: "user updated", status: 200
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end 
+  end
+  
+  def destroy 
+    if @user.destroy
+      render json: "user deleted", status: 200
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end 
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :bio, :location, :admin)
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/create'
+  get 'users/update'
+  get 'users/destroy'
   resources :plants
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
