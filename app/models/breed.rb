799999ce694430c7b65ab356ff302938f4aabf08
nova <alexander.coole@protonmail.com>
2020-07-15 10:50:43 +1000

    added breed model tests

--- app/models/breed.rb
+++ app/models/breed.rb
@@ -1,3 +1,6 @@
 class Breed < ApplicationRecord
     has_many :plants
+    validates :name, :description, :max_growth, presence: true
+    validates :max_growth, numericality: { greater_than: 10 }
+    validates :description, length: { minimum: 20 }
 end
--- spec/models/breed_spec.rb
+++ spec/models/breed_spec.rb
@@ -2,4 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Breed, type: :model do
   subject {build(:breed)}
+
+  context 'validations' do
+    it 'has a valid factory' do
+      expect(build(:breed)).to be_valid
+    end
+
+    it { expect(subject).to validate_presence_of(:name) }
+    it { expect(subject).to validate_presence_of(:description) }
+    it { expect(subject).to validate_presence_of(:max_growth) }
+    it { expect(subject).to validate_length_of(:description).is_at_least(20).on(:create) }
+    it { expect(subject).to validate_numericality_of(:max_growth).is_greater_than(10) }
+
+  end
 end
