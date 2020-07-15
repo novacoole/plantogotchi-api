4869d8befb036309954c653db6a5550e434f35d4
nova <alexander.coole@protonmail.com>
2020-07-15 14:32:56 +1000

    fixed typo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      render json: "plant created" , status: :created
+      render json: "user created" , status: :created
     else
       render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
     end
