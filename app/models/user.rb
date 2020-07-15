f91b901e641e5f2a55c663eb803704adff279722
nova <alexander.coole@protonmail.com>
2020-07-15 14:00:54 +1000

    removed uneccessary migrations
    "

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     validates :username, :password, :email, presence: true  
     validates :username, uniqueness: true, length: { minimum: 3 }
-    validates :password, length: { minimum: 8 }
+    has_secure_password
     validates :email, email: true
 
     has_many :plants
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,7 +12,6 @@ RSpec.describe User, type: :model do
     it { expect(subject).to validate_presence_of(:password) }
     it { expect(subject).to validate_presence_of(:email) }
     it { expect(subject).to validate_length_of(:username).is_at_least(3).on(:create) }
-    it { expect(subject).to validate_length_of(:password).is_at_least(8).on(:create) }
 
     it 'is not valid with an email without @ symbol' do
       subject.email = 'emailatemail.com'
