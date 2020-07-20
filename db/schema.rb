74f9bd86f573d6a75f065fad6170d5b33230533e
PING NGE <ping.nge@hotmail.com>
2020-07-20 21:32:09 +1000

    refactored sign up component

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_15_034845) do
+ActiveRecord::Schema.define(version: 2020_07_14_055016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,14 +39,13 @@ ActiveRecord::Schema.define(version: 2020_07_15_034845) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.string "password_digest"
     t.string "email"
     t.text "bio"
     t.string "location"
     t.boolean "admin"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "password"
-    t.string "password_digest"
   end
 
   add_foreign_key "plants", "breeds"
