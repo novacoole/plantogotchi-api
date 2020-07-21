95fd80b21e053ea0a4cbfbbeeec98efb54da97ed
nova <alexander.coole@protonmail.com>
2020-07-21 21:20:15 +1000

    updated user update and show routes to rely on current_user instead of :id params

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   post "/login", to: "user_token#create"
   post "/sign-up", to: "users#create"
+  get "/myaccount", to: "users#show"
+  put "/updateuser", to: "users#update"
   get "/status", to: "status#index"
   get "/status/user", to: "status#user"
   resources :plants, :breeds
-  resources :users, :except => [:create]
+  resources :users, :except => [:create, :show, :update]
 end
