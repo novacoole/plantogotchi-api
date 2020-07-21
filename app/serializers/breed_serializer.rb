214c94defecf87a3e24c108148e8a24847e2a8e9
nova <alexander.coole@protonmail.com>
2020-07-21 17:50:16 +1000

    created serializer to output url in json

--- app/serializers/breed_serializer.rb
+++ app/serializers/breed_serializer.rb
@@ -1,11 +1,9 @@
 class BreedSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
-  attributes :id, :name, :description, :max_growth, :spritesheet
+    
+  attributes :name, :description, :max_growth, :spritesheet
+  
   def spritesheet
-    if object.spritesheet.attached?
-      {
-        url: rails_blob_url(object.spritesheet)
-      }
-    end
+    url_for(object.spritesheet) if object.spritesheet.attached?
   end
 end
