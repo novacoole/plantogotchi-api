cd2089298a152d0e70733f111282580da72c3924
nova <alexander.coole@protonmail.com>
2020-07-24 12:04:18 +1000

    added plant serializer to include all needed attributes in one api call

---
+++ app/serializers/plant_serializer.rb
@@ -0,0 +1,11 @@
+class PlantSerializer < ActiveModel::Serializer
+  attributes :name, :water_level, :food_level, :breed_id, :user_id, :alive, :growth_stage, :created_at, :updated_at, :breed, :events
+  
+  def events
+    object.events.last(10)
+  end
+
+  def breed
+    BreedSerializer.new(object.breed)
+  end
+end
