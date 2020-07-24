c3758d5fe623e8d8e3073fb13e7b854471b72ab4
nova <alexander.coole@protonmail.com>
2020-07-24 11:49:35 +1000

    created event model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_21_040114) do
+ActiveRecord::Schema.define(version: 2020_07_24_001102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,15 @@ ActiveRecord::Schema.define(version: 2020_07_21_040114) do
     t.integer "max_growth"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.integer "event_type"
+    t.integer "amount"
+    t.bigint "plant_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["plant_id"], name: "index_events_on_plant_id"
   end
 
   create_table "plants", force: :cascade do |t|
